3a3596d3aaf229ed4b6794959cc906200b58ec29
Yukitooo <yukibrent9@gmail.com>
2026-01-22 23:31:17 +0800

    added some comments for future coding

---
+++ src/lib/components/TaskCard.svelte
@@ -0,0 +1,6 @@
+
+
+//Card shows: Description, Task Name, When created.
+// Data will come from database postgresql
+// If card is clicked, it will redirect to main page with new task set. 
+// Card will show if user click, "Share card"
